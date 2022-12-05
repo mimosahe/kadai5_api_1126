da324564a60f04c8a8cabc8a718a72c943369bfd
mimosahe <115439846+mimosahe@users.noreply.github.com>
2022-12-05 10:10:56 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Weather from "./components/Weather";
+import {Weather} from "./components/Weather";
 import Schedule from "./components/Schedule";
+import Date from "./components/Date";
 
 const App = () => {
   return (
     <>
       <h2>きょうの予定</h2>
+      <Date />
       <Weather />
       <Schedule />
     </>
