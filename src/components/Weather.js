21061a517e26d9e67e82559c79aa6449a0c93309
mimosahe <115439846+mimosahe@users.noreply.github.com>
2022-12-05 10:11:29 +0900

    second commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,13 @@
 import React from "react";
 import axios from "axios";
+import { useState } from "react";
 
-class Weather extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {location: "現在地", description: "きょうの天気について説明します", icon: ""};
-  }
+export const Weather = () => {
+  const [cityName, setCityName] = useState("現在地");
+  const [description, setDescription] = useState(null);
+  const [icon, setIcon] = useState(null);
 
-  handleClick = () => {
+  const handleClick = () => {
     // 現在地の緯度経度を取得（GoogleMap: Javascript API）
     function mapSuccess(position) {
       const lat = position.coords.latitude;
@@ -15,53 +15,47 @@ class Weather extends React.Component {
 
       // 緯度経度が取得できた場合は、市区町村名を取得（GoogleMap: Geocoding API）
       axios
-      .get(
-        "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyATHkVtCki6nWBWZlQNK4fOd-uFp2dkJ2g"
-      )
-      .then((response) => {
-        let results = response.data.results[0].address_components.filter(function(component) {
-          return component.types.indexOf("locality") > -1;
-        }); // 市区町村は"locality"
-        let cityName = results[0].long_name;
-        // this.setState({location: cityName});
-        console.log(cityName);
-      })
-      .catch((error) => {
-        alert(error);
-      });
+        .get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        .then((response) => {
+          let results = response.data.results[0].address_components.filter(function (component) {
+            return component.types.indexOf("locality") > -1;
+          }); // 市区町村は"locality"
+          setCityName(results[0].long_name);
+        })
+        .catch((error) => {
+          alert(error);
+        });
 
       // 緯度経度が取得できた場合は、本日の天気情報を取得（OpenWeather API）
       axios
-      .get(
-        "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=ac86a7c7f7e307421b5975639d34002d"
-      )
-      .then((response) => {
-        let description = response.data.weather[0].description
-        let icon = response.data.weather[0].icon + "@2x.png"
-        console.log(description, icon)
-      })
-      .catch((error) => {
-        alert(error);
-      });
+        .get("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        .then((response) => {
+          let description = response.data.weather[0].description;
+          let iconImage = response.data.weather[0].icon + "@2x.png";
+          setIcon(iconImage);
+          setDescription(description);
+        })
+        .catch((error) => {
+          alert(error);
+        });
     }
 
     function mapFail(error) {
       alert("位置情報の取得に失敗しました。エラーコード：" + error.code);
     }
     navigator.geolocation.getCurrentPosition(mapSuccess, mapFail);
-
   };
 
-  render() {
-    return (
-      <>
-        <button onClick={this.handleClick}>かくにんする</button>
-        <p>{this.state.location}の天気</p>
-        <p>{this.state.description}</p>
-        <div><img src={this.state.icon} /></div>
-      </>
-    );
-  }
-}
-
-export default Weather;
+  return (
+    <>
+      <button onClick={handleClick}>かくにんする</button>
+      <p>{cityName}の天気</p>
+      <p>{description}</p>
+      {icon && (
+        <div>
+          <img src={`http://openweathermap.org/img/wn/${icon}`} />
+        </div>
+      )}
+    </>
+  );
+};
